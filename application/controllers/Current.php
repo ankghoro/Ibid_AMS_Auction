b2fada21f45e29306b43b013307d1d1c3f4e674f
akmal.m <akmal.m@smooets.com>
2017-12-08 09:14:58 +0700

    add current biding controller

---
+++ application/controllers/Current.php
@@ -0,0 +1,101 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Current extends CI_Controller {
+	
+ 	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('lot_model','lot');
+        $this->load->helper(array('url','cookie'));
+        $this->load->helper('domain_helper');
+    }
+
+    private function check_token($token)
+    {
+        $url = $this->config->item('adms_auth')['check'];
+        $data = json_decode($this->postCURL($url, ["access_token" => $token]));
+        if (isset($data->error)) {
+            return false;
+        } else {
+            return $data->success;
+        }
+    }
+
+    /**
+     * function for parameter refresh token
+     * @author akmal.m@smooets.com
+     * @return array
+    */  
+    private function refresh_params($UserLogon){
+        return [
+            "grant_type" => 'refresh_token',
+            "client_id" => 'ADMS web',
+            "client_secret" => '1234567890',
+            "refresh_token" => $UserLogon['refresh_token'],
+            "username" => $UserLogon['username'],
+            "ipAddress" => '127.0.0.1',
+            "createdOn" => '1509330606'
+        ];
+    }
+    
+    /**
+     * function for refresh token
+     * @author akmal.m@smooets.com
+     * @return object
+    */
+    private function refresh_token($UserLogon)
+    {
+        $url = $this->config->item('adms_auth')['login'];
+        $data = json_decode($this->postCURL($url, $this->refresh_params($UserLogon)));
+        return $data;
+    }
+
+
+    private function postCURL($_url, $_param){
+        $postData = '';
+            //create name value pairs seperated by &
+            foreach($_param as $k => $v) 
+            { 
+               $postData .= $k . '='.$v.'&'; 
+            }
+            rtrim($postData, '&');
+        
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$_url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_HEADER, false); 
+            curl_setopt($ch, CURLOPT_POST, count($postData));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+            
+            $output=curl_exec($ch);
+            curl_close($ch);
+            return $output;
+    }
+
+    private function get_curl($url)
+    {
+        //  Initiate curl
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set the url
+        curl_setopt($ch, CURLOPT_URL,$url);
+        // Execute
+        $result=curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+	public function index()
+	{
+        echo "current works";
+	}
+
+	public function bidding()
+	{
+        echo "current bidding works";
+	}
+}
+
+?>
