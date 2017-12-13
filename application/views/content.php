89842e045a875c223776ec91d8bd899bf906b537
akmal.m <akmal.m@smooets.com>
2017-12-13 16:54:30 +0700

    tidy up html of auction

--- application/views/content.php
+++ application/views/content.php
@@ -2,9 +2,7 @@
     <div class="content-page">
         <div class="content">
             <div class="container" id="body">
-                <div class="text-center" id="loader">
-    
-                </div>
+                <div class="text-center" id="loader"></div>
                 <div class="row gap-2" id="content">
                     <div class="col-md-6 col-lg-3">
                         <div class="card">
@@ -79,60 +77,60 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 center-align">
+                        <div class="card">
+                            <div class="card-block block-content">
+                                <div class="fold-price">
+                                    Bidding Log
+                                    </div>
+                            <div class="bidding-log" style="border: 1px solid #cccccc !important">
+                                <div class="row no-gutters line-height" id="bid-log">
+                                </div>
                             </div>
-                                <div class="col-md-6 col-lg-3 center-align">
-                                    <div class="card">
-                                        <div class="card-block block-content">
-                                            <div class="fold-price">
-                                                Bidding Log
-                                             </div>
-                                        <div class="bidding-log" style="border: 1px solid #cccccc !important">
-                                            <div class="row no-gutters line-height" id="bid-log">
-                                            </div>
-                                        </div>
-                                        </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-block block-content center-align">
+                                <div class="fold-price">
+                                    Control
+                            </div> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group clearfix">
+                                        <label class="control-label">Nomor Lot:</label>
+                                        <input type="text" class="form-control" id="skip">
+                                            <button class="btn btn-warning btn-sm btn-block" id="btn_skip">SKIP</button>
+                                    </div>
+                                    <div class="form-group clearfix">
+                                        <label class="control-label">Bidding Control</label>
+                                        <button class="btn btn-success btn-sm btn-block" style="height:100px;" id="floor-bid" disabled="disabled">+
+                                        </button>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-3">
-                                    <div class="card">
-                                         <div class="card-block block-content center-align">
-                                            <div class="fold-price">
-                                                Control
-                                            </div> 
-                                            	<div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group clearfix">
-                                                            <label class="control-label">Nomor Lot:</label>
-                                                            <input type="text" class="form-control" id="skip">
-                                                             <button class="btn btn-warning btn-sm btn-block" id="btn_skip">SKIP</button>
-                                                        </div>
-                                                        <div class="form-group clearfix">
-                                                            <label class="control-label">Bidding Control</label>
-                                                            <button class="btn btn-success btn-sm btn-block" style="height:100px;" id="floor-bid" disabled="disabled">+
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group clearfix">
-                                                            <button class="btn btn-info btn-sm btn-block" id="start">START</button>
-                                                        </div>
-	                                                    <div class="form-group clearfix">
-	                                                        <label class="control-label">Count</label>
-	                                                        <input type="text" class="form-control" style="height: 100px;" id="count" value="0" readonly>
-	                                                        <button class="btn btn-success btn-sm btn-block" id="btn_count" disabled="disabled">COUNT</button>
-	                                                        <button class="btn btn-warning btn-sm btn-block" id="btn_next">NEXT</button>
-	                                                    </div>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <a target="_blank" href="<?php echo base_url('current/bidding');?>" class="btn btn-primary btn-sm btn-block">Current Bidding</a>
-                                                    </div>
-                                                </div>
-                                              </div>
-                                            </div>
-                                          </div>
-                                  </div>
+                                <div class="col-md-6">
+                                    <div class="form-group clearfix">
+                                        <button class="btn btn-info btn-sm btn-block" id="start">START</button>
+                                    </div>
+                                    <div class="form-group clearfix">
+                                        <label class="control-label">Count</label>
+                                        <input type="text" class="form-control" style="height: 100px;" id="count" value="0" readonly>
+                                        <button class="btn btn-success btn-sm btn-block" id="btn_count" disabled="disabled">COUNT</button>
+                                        <button class="btn btn-warning btn-sm btn-block" id="btn_next">NEXT</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <a target="_blank" href="<?php echo base_url('current/bidding');?>" class="btn btn-primary btn-sm btn-block">Current Bidding</a>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
