5933f2af04d88f8f71805435e0626243cdd05534
akmal.m <akmal.m@smooets.com>
2017-12-16 22:28:34 +0700

    update layout

--- application/views/current-bidding/index.php
+++ application/views/current-bidding/index.php
@@ -4,16 +4,16 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 col-lg-6">
-                        <div class="card purple-bold">
+                        <div class="card purple-verybold">
                             <div class="bid-name">
                                 <div class="row">
-                                    <div class="col-md-3">
+                                    <div class="col-3">
                                         <div class="side-lot">
                                             <h6 class="sub-title">LOT</h6>
                                             <h4 class="lot-number">80</h4>
                                         </div>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-9">
                                         <h6 class="main-title">Ford Escort 1,5 Titanium Mini Bus</h6>
                                     </div>
                                 </div>
@@ -25,29 +25,31 @@
                                             <span class="grade-text">Grade</span> <br>
                                             <span class="grade-alpha">A</span>
                                         </div>
-                                        <img class="card-img-top" src="assets/img/ford.jpg" style="max-width: 100%;">
+                                        <div class="card-img-top"></div>
                                     </div>
                                 </div>
-                                <div class="purple-bold">
-                                    <div class="row">
-                                        <div class="col-md-6">
+                                <div class="purple-verybold border-bottom">
+                                    <div class="row margin0 purple-bold padding12">
+                                        <div class="col-6 separator1">
                                             <h6>Tahun</h6>
-                                            <h5>2011</h5>
+                                            <h5 class="bold">2011</h5>
                                         </div>
-                                        <div class="col-md-6">
+                                        <div class="col-6">
                                             <h6>Harga Awal</h6>
-                                            <h5>Rp. 200.000.000</h5>
+                                            <h5 class="bold">Rp. 339,000,000</h5>
                                         </div>
+                                    </div>
+                                    <div class="row">  
                                         <div class="col-md-6">
                                             <div class="rounded-iden">
-                                                Nomor Polisi
-                                                <h6>B 1401 MJ</h6>
+                                            Nomor Polisi
+                                            <h6>B 1401 MJ</h6>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="rounded-iden">
-                                                Nomor Polisi
-                                                <h6>B 1401 MJ</h6>
+                                            Nomor Polisi
+                                            <h6>B 1401 MJ</h6>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
@@ -58,8 +60,8 @@
                                         </div>
                                         <div class="col-md-6">
                                             <div class="rounded-iden">
-                                                Transmisi
-                                                <h6>AT</h6>
+                                            Transmisi
+                                            <h6>AT</h6>
                                             </div>
                                         </div>
                                     </div>
@@ -69,54 +71,60 @@
                     </div>
                     <div class="col-md-6 col-lg-6">
                         <div class="card purple-verybold">
-                        <div class="card-block block-content">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <center>
-                                        <div class="bid-count">
-                                        <h6>COUNT</h6>  
-                                        1
+                            <div class="card-block block-content">
+                                <div class="row margin11">
+                                    <div class="col-md-3">
+                                        <center>
+                                            <div class="bid-count">
+                                                <h6 class="count-right">COUNT</h6>  
+                                                1
+                                            </div>
+                                        </center>
+                                    </div>
+                                    <div class="col-md-9  padding11 color-right">
+                                        <div class="young-purple">
+                                            <div class="center-content">
+                                                <img src="assets/img/star.png" class="img-top">&nbsp <span class="top-bidder">TOP BIDDER</span>
+                                            </div>
+                                            <div class="bid-topbid">Rp. -</div>
+                                            <div class="pull-right">-</div>
                                         </div>
-                                    </center>
+                                    </div>
                                 </div>
-                                <div class="col-md-9 young-purple padding11">
-                                    <img src="assets/img/star.png" class="img-top">&nbsp <span class="top-bidder">TOP BIDDER</span>
-                                    <div class="bid-topbid">Rp. -</div>
-                                    <div class="pull-right">-</div>
-                                </div>
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="stock-detail padding13">
+                                            <div class="row padding10">
+                                                <div class="col-6 col-md-12 item">
+                                                    <h6 class="title-caption">Exterior</h6>
+                                                    <h5 class="main-caption weight">-</h5>
+                                                </div> 
+                                                <div class="col-6 col-md-12 item">
+                                                    <h6 class="title-caption">Interior</h6>
+                                                    <h5 class="main-caption weight">A</h5>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6 col-md-12 item">
+                                                    <h6 class="title-caption">Mechanical</h6>
+                                                    <h5 class="main-caption weight">B</h5>
+                                                </div>
+                                                <div class="col-6 col-md-12 item">
+                                                    <h6 class="title-caption">Frame</h6>
+                                                    <h5 class="main-caption weight">A</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-9 padding10">
+                                        <div class="bidding-log">
+                                        </div>
+                                        <div class="fold-price">
+                                            Harga Kelipatan: Rp. 500,000
+                                        </div>
+                                    </div>
+                                </div>                   
                             </div>
-                            <div class="row">
-                                <div class="col-md-3">                                               
-                                    <div class="stock-detail">
-                                        <div class="row padding10">
-                                            <div class="col-12 item">
-                                                <h6 class="title-caption">Exterior</h6>
-                                                <h5 class="main-caption weight">A</h5>
-                                            </div>
-                                            <div class="col-12 item">
-                                                <h6 class="title-caption">Interior</h6>
-                                                <h5 class="main-caption weight">A</h5>
-                                            </div>
-                                            <div class="col-12 item">
-                                                <h6 class="title-caption">Mechanical</h6>
-                                                <h5 class="main-caption weight">B</h5>
-                                            </div>
-                                            <div class="col-12 item">
-                                                <h6 class="title-caption">Frame</h6>
-                                                <h5 class="main-caption weight">A</h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-9 padding10">
-                                    <div class="bidding-log">
-                                    </div>
-                                    <div class="fold-price">
-                                        Harga Kelipatan: Rp. 500.000
-                                    </div>
-                                </div>
-                            </div>                
-                        </div>
                         </div>
                     </div>
                 </div>
