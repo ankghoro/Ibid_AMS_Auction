be7a9207f0688f46401016ec4418c0d676eee332
Panji Pamungkas <panjipamungkas309@gmail.com>
2017-12-23 10:37:25 +0700

    Updates current bidding feedbacsk

--- application/views/templates/current-bid.php
+++ application/views/templates/current-bid.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-  <title>AMS | Current Bidding</title>
+  <title>Current Bidding - IBID AMS</title>
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('current/assets/css/bootstrap.min.css')?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('current/assets/css/style.css')?>">
