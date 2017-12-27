effd0c3af937b2ac72fe50a08ef3527f4aa01b02
Panji Pamungkas <panjipamungkas309@gmail.com>
2017-12-27 13:09:55 +0700

    Added online bidding input data log via firebase

---
+++ application/controllers/Online.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Online extends CI_Controller {
+	
+ 	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('online_bid','bid');
+    }
+
+	public function index()
+	{
+
+	}
+
+	public function bid()
+	{
+        $interval = (int)$this->input->post('interval');
+        $biddertype = "Online Bidder";
+        $company = $this->input->post('company');
+        $schedule = $this->input->post('schedule');
+        $lotStock = $this->input->post('stock');
+        $database = $this->bid->firebase()->getDatabase();
+        $reference = $database->getReference("company/$company/schedule/$schedule/mode|ON/lot|stock/$lotStock/log");
+        // var_dump($reference);die();
+        $postData = [
+            "bid" => $interval,
+            "type" => $biddertype
+        ];
+        $reference->push($postData);
+
+
+
+        return $this->output
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode(["status" => true]));
+    }
+
+}
+
+?>
