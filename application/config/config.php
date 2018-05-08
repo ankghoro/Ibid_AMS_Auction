fc821cca22c11dc3c1724256c35bc35f8f365196
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-05-08 18:34:52 +0700

    add log

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
