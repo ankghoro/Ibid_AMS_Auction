65be1b7191b215650ae00d26ff138e1faac7a77e
Suvi Sanusi <suvi.7888@gmail.com>
2018-05-14 13:53:01 +0700

    xml trigger-base npl kalah lelang

--- application/views/script.php
+++ application/views/script.php
@@ -1037,8 +1037,14 @@ function doneCurrentSchedule(id){
         url: "<?php echo $this->config->item('ibid_npl');?>/counter/npl/kalah?sch="+id+"", // updateNplKalah
         success: function(updateDataNpl){}
       });
+      
+      $.ajax({
+        type: "POST",
+        url: "<?php echo $this->config->item('ibid_npl');?>/counter/npl/npllostxml", // xmlNplKalah
+        success: function(updateDataNpl){}
+      });
 			
-			$.ajax({
+      $.ajax({
         type: "POST",
         url: "<?php echo $this->config->item('ibid_stock');?>/itemStock/AuctionLose?sch="+id+"", // updateStokKalah
         success: function(updateDataNpl){}
